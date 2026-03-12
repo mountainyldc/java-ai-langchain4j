ddefb43a336b8e43a5f68b7a7d04df37350a91db
yld <2423141850@qq.com>
2026-03-12 14:39:47 +0800

    commit

--- src/main/java/com/atguigu/java/ai/langchain4j/assistant/Assistant.java
+++ src/main/java/com/atguigu/java/ai/langchain4j/assistant/Assistant.java
@@ -1,5 +1,5 @@
 package com.atguigu.java.ai.langchain4j.assistant;
 
 public interface Assistant {
-    String chat(String userMessage); // 接收用户输入
+    String chat(String userMessage); // 接收用户输入111
 }
